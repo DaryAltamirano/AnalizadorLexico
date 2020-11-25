1a6bbacc555cfcaa4eebc33fb70d5748de05e34f
Darien Altamirano <darybarce16@gmail.com>
2020-11-24 20:01:33 -0500

    Interfaz terminada

--- Interfaz.py
+++ Interfaz.py
@@ -5,32 +5,29 @@ from tkinter import *
 ###METODOS DE APOYO
 
 def getTextInput():
-    texto= TextoCodigo.get(1.0,END)   
-    print(texto)
-
-
-
+     texto= TextoCodigo.get(1.0,END)   
+     LabelResultado["text"]=texto
 
 
 ####INICIO DE LA INTERFAZ
 raiz =Tk()
 raiz.title("Analizador sintáctico")
 raiz.resizable(0,0)
+# FrameInicio(raiz)
 # raiz.geometry("650x500")
 # raiz.config(bg="lightblue")
-
-
-miFrame=Frame(raiz,width="650",height="450",bg="lightblue")
+miFrame = Frame(raiz, width="650", height="700", bg="lightblue")
 miFrame.pack()
-# 
-Label(miFrame,text="Ingrese algo para el analisis",fg="red",bg="lightblue",font=("Comic Sans MS",22)).place(x=325, y=50, anchor="center")
+#
+Label(miFrame, text="Ingrese algo para el analisis", fg="red", bg="lightblue",font=("Comic Sans MS", 22)).place(x=325, y=50, anchor="center")
 # TEXTAREA
-TextoCodigo=Text(raiz,width=70,height=15,padx=10,pady=10)
+TextoCodigo = Text(miFrame, width=70, height=15, padx=10, pady=10)
 TextoCodigo.place(x=325, y=200, anchor="center")
+LabelResultado=Label(miFrame, text="Aqui ira el resultado del analisis", fg="red", bg="lightblue",font=("Arial Novas", 18))
+LabelResultado.place(x=325, y=500, anchor="center")
 # BOTONES
-botonLexico=Button(raiz,text="Analizador Lexico",command=getTextInput).place(x=150,y=400)
-botonSintactico=Button(raiz,text="Analizador Sintactico",command=getTextInput).place(x=400,y=400)
+botonLexico = Button(miFrame, text="Analizador Lexico",command=getTextInput).place(x=150, y=600)
+botonSintactico = Button(miFrame, text="Analizador Sintactico",command=getTextInput).place(x=400, y=600)
+
 
 raiz.mainloop()
-
-
